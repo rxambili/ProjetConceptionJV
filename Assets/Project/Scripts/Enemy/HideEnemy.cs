4150419399fc6b59e4684f5f2dec54bfcae63e18
rxambili <r.xambili@gmail.com>
2018-11-10 19:01:56 -0500

    Adding files

---
+++ Assets/Project/Scripts/Enemy/HideEnemy.cs
@@ -0,0 +1,42 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class HideEnemy : MonoBehaviour, IHideable
+{
+
+
+    // Use this for initialization
+    void Awake()
+    {
+        Component[] meshComponents = GetComponentsInChildren(typeof(SkinnedMeshRenderer), true);
+        foreach (SkinnedMeshRenderer m in meshComponents)
+        {
+            m.enabled = false;
+        }
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
+
+    public void OnFOVEnter()
+    {
+        Component[] meshComponents = GetComponentsInChildren(typeof(SkinnedMeshRenderer), true);
+        foreach (SkinnedMeshRenderer m in meshComponents)
+        {
+            m.enabled = true;
+        }
+    }
+
+    public void OnFOVLeave()
+    {
+        Component[] meshComponents = GetComponentsInChildren(typeof(SkinnedMeshRenderer), true);
+        foreach (SkinnedMeshRenderer m in meshComponents)
+        {
+            m.enabled = false;
+        }
+    }
+}
