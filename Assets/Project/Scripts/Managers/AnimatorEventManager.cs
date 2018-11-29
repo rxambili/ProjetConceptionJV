c282634e40d2db3b19c01be66e75035c70d08caa
Ludwiig <Zackpooo@hotmail.fr>
2018-11-29 19:57:34 +0100

    Ajout de l'UI de creation de personnage

---
+++ Assets/Project/Scripts/Managers/AnimatorEventManager.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace GameProject {
+    public class AnimatorEventManager : MonoBehaviour {
+
+        public void EndWeaponReload()
+        {
+            GetComponentInChildren<PlayerShooting>().EndReload();
+        }
+
+        public void EndWeaponFailedReload()
+        {
+            GetComponentInChildren<PlayerShooting>().EndFailedReload();
+        }
+    }
+}
---
+++ Assets/Project/Scripts/Managers/RessourcesManager.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+namespace GameProject {
+    public class RessourcesManager : MonoBehaviour {
+
+        public Text ammoText;
+
+        public int startingTotalAmmo;
+        public int currentTotalAmmo { get; set; }
+
+        // Use this for initialization
+        void Start() {
+            currentTotalAmmo = startingTotalAmmo;
+        }
+
+        // Update is called once per frame
+        void Update() {
+            ammoText.text = string.Format("{0} / {1}", GetComponentInChildren<PlayerShooting>().GetAmmo(), currentTotalAmmo);
+    
+        }
+
+        public int UseAmmo(int ammo)
+        {
+            int usedAmmo = Mathf.Min(ammo, currentTotalAmmo);
+            currentTotalAmmo -= usedAmmo;
+            return usedAmmo;
+        }
+
+    }
+}
