99ed389ba2f7142eca645cdd71d32383bed0f7e5
Ludwiig <Zackpooo@hotmail.fr>
2018-12-07 00:07:01 +0100

    Calcul des statistiques + Gestion de la limitation du nombre de slot + Bouton ajout de slot

---
+++ Assets/DeverrouilleSlot.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DeverrouilleSlot : MonoBehaviour {
+    public int orbes = 1500;
+    public GameObject pauvreTxt;
+    public GameObject pasPauvreTxt;
+    public Transform AssemblyPanel;
+    GameObject pasPauvreTxt1;
+    GameObject pauvreTxt1;
+    CharacterCreationManager characterUi;
+    void Start()
+    {
+        characterUi = GetComponentInParent<CharacterCreationManager>();
+    }
+    public void deverouilleSlot()
+    {
+        if(orbes >= 1500)
+        {
+            characterUi.nbSlot++;
+            orbes -= 1500;
+            pasPauvreTxt1 = Instantiate(pasPauvreTxt, AssemblyPanel);
+            Destroy(pasPauvreTxt1, 2);
+        }
+        else
+        {
+            pauvreTxt1 = Instantiate(pauvreTxt, AssemblyPanel);
+            Destroy(pauvreTxt1, 2);
+        }
+    }
+}
