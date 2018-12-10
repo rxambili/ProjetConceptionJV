ad41a0602b226a893595226a83b0224227ef57c3
Ludwiig <Zackpooo@hotmail.fr>
2018-12-10 04:56:35 +0100

    Interface Menu Principale : Boutton "Quitter" et "Jouer"

---
+++ Assets/QuitScript.cs
@@ -0,0 +1,12 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class QuitScript : MonoBehaviour {
+
+    public void ButtonQuit()
+    {
+        Debug.Log("Quitter l'application : a implémenter !");
+        Application.Quit();
+    }
+}
