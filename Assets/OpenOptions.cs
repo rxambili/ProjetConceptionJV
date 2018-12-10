f1b049714b0d37d5d4d4aa81557474c91880d35e
Ludwiig <Zackpooo@hotmail.fr>
2018-12-10 21:06:48 +0100

    Interface MenuPrincipal : Ajout Modificateur de Volume / Effet

---
+++ Assets/OpenOptions.cs
@@ -0,0 +1,13 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class OpenOptions : MonoBehaviour {
+
+    public GameObject OptionsPanel;
+
+    public void OpenOptionPanelButton()
+    {
+        OptionsPanel.SetActive(true);
+    }
+}
