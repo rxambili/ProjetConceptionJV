d7a9807ea9eee9c2547e6791d1dd22dbc10d3d43
rxambili <r.xambili@gmail.com>
2018-12-10 17:24:29 -0500

    fix: changement audio slug shout et hit player

--- Assets/Scripts/Helpers/ShoutingSlug.cs
+++ Assets/Scripts/Helpers/ShoutingSlug.cs
@@ -11,8 +11,14 @@ public class ShoutingSlug : StateMachineBehaviour {
 	 // OnStateEnter is called when a transition starts and the state machine starts to evaluate this state
 	override public void OnStateEnter(Animator animator, AnimatorStateInfo stateInfo, int layerIndex) {
         animator.GetComponent<EnemySlugHealth>().isShouting = true;
+        audioSource = animator.GetComponent<AudioSource>();
+        if (audioSource.isPlaying)
+        {
+            audioSource.Stop();
+        }
+        audioSource.clip = audioClips[Random.Range(0, audioClips.Length - 1)];
+        audioSource.Play();
 
-        
     }
 
 	// OnStateUpdate is called on each Update frame between OnStateEnter and OnStateExit callbacks
@@ -23,13 +29,6 @@ public class ShoutingSlug : StateMachineBehaviour {
 	// OnStateExit is called when a transition ends and the state machine finishes evaluating this state
 	override public void OnStateExit(Animator animator, AnimatorStateInfo stateInfo, int layerIndex) {
         animator.GetComponent<EnemySlugHealth>().isShouting = false;
-        audioSource = animator.GetComponent<AudioSource>();
-        if (audioSource.isPlaying)
-        {
-            audioSource.Stop();
-        }
-        audioSource.clip = audioClips[Random.Range(0, audioClips.Length - 1)];
-        audioSource.Play();
     }
 
 	// OnStateMove is called right after Animator.OnAnimatorMove(). Code that processes and affects root motion should be implemented here
